4e7015bd90e1aecf2fe27bec61b47e4ffce8178a
killer <77133451+990052053@users.noreply.github.com>
2021-04-14 15:51:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,11 +13,11 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# Add custom feeds source into feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
-# Add custom feeds source
+# Clone custom feeds source
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb #git lua-maxminddb 依赖
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 
